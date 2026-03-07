1c7973d052bca0572597c22eda54a7bb0c967fa1
Juan Carlos <jcarlosvazbeas@gmail.com>
2026-03-06 18:59:48 -0600

    Disable ST7789 color inversion to restore true black theme

--- main/display_st7789_lvgl.c
+++ main/display_st7789_lvgl.c
@@ -258,7 +258,7 @@ lv_display_t* display_st7789_lvgl_init(void)
 
     ESP_ERROR_CHECK(esp_lcd_panel_set_gap(panel_handle, LCD_X_GAP, LCD_Y_GAP));
 #if LCD_INVERT_COLOR
-    ESP_ERROR_CHECK(esp_lcd_panel_invert_color(panel_handle, true));
+    ESP_ERROR_CHECK(esp_lcd_panel_invert_color(panel_handle, false));
 #endif
     ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
 
